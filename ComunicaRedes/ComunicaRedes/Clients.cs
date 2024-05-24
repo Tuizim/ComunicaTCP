8d76e49b83cef41e15871215a067c2d101e77c2d
Tuizim <tuitrombim@gmail.com>
2024-05-24 14:53:24 -0300

    Somente para teste

---
+++ ComunicaRedes/ComunicaRedes/Clients.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class TcpClientProgram
+{
+    private static TcpClient _client;
+    private static NetworkStream _stream;
+
+    static void Main()
+    {
+        try
+        {
+            _client = new TcpClient("127.0.0.1", 5000);
+            _stream = _client.GetStream();
+            Console.WriteLine("Conectado ao servidor. Você pode começar a enviar mensagens.");
+
+            Thread readThread = new Thread(ReadMessages);
+            readThread.Start();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message == "exit")
+                {
+                    break;
+                }
+
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                _stream.Write(data, 0, data.Length);
+            }
+
+            _client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro: " + e.Message);
+        }
+    }
+
+    private static void ReadMessages()
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        try
+        {
+            while ((bytesRead = _stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                Console.WriteLine("Recebido: "+Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro: " + e.Message);
+        }
+    }
+}
---
+++ ServerRedes/ServerRedes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class TcpServer
+{
+    private static TcpListener _listener;
+    private static TcpClient _client1;
+    private static TcpClient _client2;
+
+    static void Main()
+    {
+        _listener = new TcpListener(IPAddress.Any, 5000);
+        _listener.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        _listener.Start();
+        Console.WriteLine("Servidor iniciado. Aguardando conexões...");
+
+        _client1 = _listener.AcceptTcpClient();
+        Console.WriteLine("Cliente 1 conectado.");
+
+        _client2 = _listener.AcceptTcpClient();
+        Console.WriteLine("Cliente 2 conectado.");
+
+        Thread client1Thread = new Thread(() => HandleClient(_client1, _client2));
+        Thread client2Thread = new Thread(() => HandleClient(_client2, _client1));
+
+        client1Thread.Start();
+        client2Thread.Start();
+
+        client1Thread.Join();
+        client2Thread.Join();
+
+        _client1.Close();
+        _client2.Close();
+        _listener.Stop();
+    }
+
+    private static void HandleClient(TcpClient client, TcpClient otherClient)
+    {
+        NetworkStream stream = client.GetStream();
+        NetworkStream otherStream = otherClient.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        try
+        {
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                otherStream.Write(buffer, 0, bytesRead);
+                Console.WriteLine("Cliente: "+Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro: " + e.Message);
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
